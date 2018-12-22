480eb16b890725dee10d5d998e8c4b18ecce44e8
Jose Figueroa <Josefigueroalv@protonmail.com>
2018-12-22 06:30:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
+const Child = ({match}) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <h2>Accounts</h2>
+          <ul>
+            <li><Link to='/netflix'>Netflix</Link></li>
+            <li><Link to='/zillow-group'>Zillow Group</Link></li>
+            <li><Link to='/yahoo'>Yahoo</Link></li>
+            <li><Link to='/modus-create'>Modus Create</Link></li>
+          </ul>
+
+          <Route path='/:id' component={Child} />
+        </div>
+      </Router>
     );
   }
 }
